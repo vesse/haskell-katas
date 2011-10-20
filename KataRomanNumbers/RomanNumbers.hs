06ae84990727069a38da6a0bf16bfce859a72af0
Vesa Poikajärvi <vesa.poikajarvi@digia.com>
2011-10-20 13:13:02 +0300

    More tests and more code

--- KataRomanNumbers/RomanNumbers.hs
+++ KataRomanNumbers/RomanNumbers.hs
@@ -2,4 +2,13 @@
 module KataRomanNumbers.RomanNumbers where
 
 toromans :: Int -> String
-toromans x = undefined
+toromans 0 = ""
+toromans x
+  | triggers 10 = (take (countof 10) $ repeat 'X') ++ (toromans (contwith 10))
+  | triggers 5  = (take (countof 5)  $ repeat 'V') ++ (toromans (contwith 5))
+  | otherwise   = if x == 4
+                  then "IV"
+                  else take x $ repeat 'I'
+  where countof y  = floor $ fromIntegral(x) / fromIntegral(y)
+        triggers y = countof y > 0
+        contwith y = x - y * (countof y)
--- Test/RomanNumbersTest.hs
+++ Test/RomanNumbersTest.hs
@@ -6,5 +6,23 @@ import KataRomanNumbers.RomanNumbers (toromans)
 
 toromans_specs = describe "toromans" [
   it "returns and empty string when given 0"
-    (toromans 0 == "")
+    (toromans 0 == ""),
+
+  it "returns \"I\" when given 1"
+    (toromans 1 == "I"),
+
+  it "returns \"V\" when given 5"
+    (toromans 5 == "V"),
+
+  it "returns \"X\" when given 10"
+    (toromans 10 == "X"),
+
+  it "returns \"XV\" when given 15"
+    (toromans 15 == "XV"),
+
+  it "returns \"IV\" when given 4"
+    (toromans 4 == "IV"),
+
+  it "returns \"IX\" when given 9"
+    (toromans 9 == "IX")
   ]
