7901ce9b4dc9b16783419a1fd69f003ccee0615b
Vesa Poikajärvi <vesa.poikajarvi@digia.com>
2011-10-21 13:56:35 +0300

    More tests and of course more code for DictionaryReplace kata

--- KataDictReplacer/DictReplacer.hs
+++ KataDictReplacer/DictReplacer.hs
@@ -2,6 +2,37 @@
 module KataDictReplacer.DictReplacer where
 
 import qualified Data.Map as Map
+import qualified Data.List as List (List.elemIndex)
+
+notFound = -1
 
 dictreplace :: String -> Map.Map String String -> String
-dictreplace s m = undefined
+dictreplace [] _ = []
+dictreplace s m  =
+  let
+    s_idx = getidx s
+    e_idx = getidx $ drop (s_idx + 1) s
+
+  in if s_idx /= notFound && e_idx /= notFound
+     then
+       (take s_idx s) ++
+       (replkw (take e_idx $ drop (s_idx + 1) s) m) ++
+       (dictreplace (drop (s_idx + e_idx + 2) s) m)
+     else s
+
+-- Using Maybe could be more "Haskell" but we need ints. Also, to get the
+-- actual int from a Maybe value would require including additional module
+-- to get access to function fromMaybe
+getidx :: String -> Int
+getidx [] = notFound
+getidx s  =
+  case '$' `List.elemIndex` s of
+    Just idx -> idx
+    Nothing  -> notFound
+
+replkw :: String -> Map.Map String String -> String
+replkw [] _ = []
+replkw s m  =
+  case Map.lookup s m of
+    Just val -> val
+    Nothing  -> '$' : s ++ "$"
--- Test/DictReplacerTest.hs
+++ Test/DictReplacerTest.hs
@@ -5,7 +5,22 @@ import qualified Data.Map as Map
 
 import KataDictReplacer.DictReplacer
 
+dict = Map.fromList [
+  ("temp", "temporary"),
+  ("name", "John Doe")
+  ]
+
+
 dictreplacer_specs = describe "dictreplace" [
   it "returns the given string if given dictionary is empty"
-    (dictreplace "FooBar" (Map.fromList []) == "FooBar")
+    (dictreplace "FooBar" Map.empty == "FooBar"),
+
+  it "returns \"temporary\" when given \"$temp$\" and a dict"
+    (dictreplace "$temp$" dict == "temporary"),
+
+  it "replaces all keywords from given string"
+    (dictreplace "hey $name$ i'm $temp$ $temp$" dict == "hey John Doe i'm temporary temporary"),
+
+  it "leaves unexisting keywords in the string"
+    (dictreplace "hey $name$ eat some $choco$" dict == "hey John Doe eat some $choco$")
   ]
